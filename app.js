50532b5773fb22c4cf59e41a1c0758c90eef80a8
Sarvesh MD <76893756+SarveshMD@users.noreply.github.com>
2025-07-04 13:11:39 +0530

    Bootstrapped, added navbar and footer using ejs-mate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campgrounds');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -18,6 +19,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
